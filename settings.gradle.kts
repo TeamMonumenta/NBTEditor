81b9c603b886545c5e038f27d48f162a602b0a6f
Byron Marohn <Combustible@live.com>
2022-12-18 18:08:10 +0000

    Drop support for 1.18.0/1.18.1
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 rootProject.name = "nbteditor"
 include(":adapter_api")
 include(":adapter_v1_16_R3")
-include(":adapter_v1_18_R1")
 include(":adapter_v1_18_R2")
 include(":NBTEditor")
 project(":NBTEditor").projectDir = file("plugin")
