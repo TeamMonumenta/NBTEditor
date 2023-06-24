bfe9110de82eadea72155828b00d6e75e4704743
U5B <56985400+U5B@users.noreply.github.com>
2023-06-23 22:17:41 -0700

    it compiles!

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "nbteditor"
 include(":adapter_api")
-include(":adapter_v1_16_R3")
 include(":adapter_v1_18_R2")
+include(":adapter_v1_19_R3")
 include(":nbteditor")
 project(":nbteditor").projectDir = file("plugin")
 
