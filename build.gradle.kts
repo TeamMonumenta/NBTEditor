07b477550bec32412416ff46ac2b93a76d22d2c1
Floweynt <guywithanaxe42@gmail.com>
2024-09-03 17:35:30 -0500

    migrate to common gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,24 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+
 plugins {
-    id("com.palantir.git-version") version "0.12.2"
+    id("com.playmonumenta.gradle-config") version "1.+"
 }
 
-description = "NBTEditor"
-val gitVersion: groovy.lang.Closure<String> by extra
-version = gitVersion()
+monumenta {
+    name("NBTEditor")
+    pluginProject(":nbteditor")
+    paper(
+        "com.goncalomb.bukkit.nbteditor.NBTEditor", BukkitPluginDescription.PluginLoadOrder.POSTWORLD, "1.18.2",
+        authors = listOf("goncalomb", "Team Monumenta"),
+        depends = listOf("CommandAPI"),
+        softDepends = listOf("MonumentaNetworkRelay")
+    )
+
+    versionAdapterApi("adapter_api", paper = "1.18.2") {
+        dependencies {
+            implementation("org.apache.commons:commons-lang3:3.0")
+        }
+    }
+    versionAdapter("adapter_v1_18_R2", "1.18.2")
+    versionAdapter("adapter_v1_19_R3", "1.19.4")
+}
