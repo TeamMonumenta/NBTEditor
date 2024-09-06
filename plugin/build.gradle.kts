a9266765209287379c95382a70fe96403a36e443
Floweynt <guywithanaxe42@gmail.com>
2024-09-06 12:35:42 -0500

    fix compiler error on test

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,4 +2,5 @@ dependencies {
     implementation(libs.bstats)
     implementation(libs.commons)
     testImplementation(libs.junit)
+    testImplementation("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
 }
